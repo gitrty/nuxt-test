1161ae98bcaa537beee1c5d4bdb2aae677423d45
Toyo <709292639@qq.com>
2020-03-12 15:01:23 +0800

    完成两种路由跳转测试

---
+++ pages/about/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>about页面</div>
+    <nuxt-link :to="{path:'/news',query:{id:'666'}}">news页</nuxt-link>
+    <nuxt-link :to="{path:'/'}">回主页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "news",
+  components: {},
+  mounted() {
+    console.info(this.$route.params);
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ pages/news/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>new页面</div>
+    <nuxt-link :to="{name:'about-id',params:{age:18}}">去about页</nuxt-link>
+    <nuxt-link :to="{path:'/'}">会主页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "news",
+  components: {},
+  mounted() {
+    console.info(this.$route.query);
+  }
+};
+</script>
+
+<style>
+</style>
+
